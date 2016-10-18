65dfaccd6e1ac81939d8dab4cb4185a9255e399d
Jennifer Varahala <Jennifer Varahala>
2016-10-18 14:23:51 -0400

    Added comments while planning

--- Node.js Basics/stage_2_video_1/app.js
+++ Node.js Basics/stage_2_video_1/app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+//Problem: We need a simple way to look at a users badge count and JavaScript points
+//solution: Use Node.js to connect to Treehousee's API to get profile information to pring out
+
+//Connect to the API URL (http://teamtreehousee.com/username.json)
+//Read the data
+//Parse the data (read data from string back to reader friendly format)
+//Print the data
