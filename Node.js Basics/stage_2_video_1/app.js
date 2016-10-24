12c570d0494ab90867b6987a7dc79d6d50c6b015
Jennifer Varahala <Jennifer Varahala>
2016-10-24 18:03:20 -0400

    Handling errors in Node

--- Node.js Basics/stage_2_video_1/app.js
+++ Node.js Basics/stage_2_video_1/app.js
@@ -1,7 +1,30 @@
 //Problem: We need a simple way to look at a users badge count and JavaScript points
 //solution: Use Node.js to connect to Treehousee's API to get profile information to pring out
 
-//Connect to the API URL (http://teamtreehousee.com/username.json)
+var http = require("http");
+var username = "chalkley";
+
+function printMessage(username, badgeCount, points) {
+	var message = username + " has " + badgeCount + " total badge(s) and " + points + "points in JavaScript";
+		console.log(message);
+}
+
+//Connect to the API URL (http://teamtreehouse.com/username.json)
+
+var request = http.get("http://teamtreehouse.com/" + username + ".json", function (response) {
+	console.log(response.statusCode);
+
 //Read the data
+
+response.on('data', function (chunk) {
+	console.log('BODY: ' + chunk);
+});
+
 //Parse the data (read data from string back to reader friendly format)
 //Print the data
+});
+
+request.on("error", function(){
+	console.error(error.message);
+
+});
